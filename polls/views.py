9e48d0c17be85284fa86a5066dc0b866bf8ecf40
Justin George <justingeorge322@gmail.com>
2024-03-26 09:42:29 +0530

    INIT

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def HomeView(request):
-    return HttpResponse("Hello, world. fcbabd58 is the polls index.")
+    return HttpResponse("Hello, world. 9a047685 is the polls index.")
